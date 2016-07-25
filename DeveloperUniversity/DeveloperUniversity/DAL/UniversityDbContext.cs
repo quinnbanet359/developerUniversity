4199b179a9a477b5c179f78e4fd46ecda3f788e7
Tyler Blake <tyler.blake.louisville@gmail.com>
2016-07-25 09:17:33 -0400

    Update webconfig and DBContext settings to connect to hosted SQL server.

--- DeveloperUniversity/DeveloperUniversity/DAL/UniversityDbContext.cs
+++ DeveloperUniversity/DeveloperUniversity/DAL/UniversityDbContext.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Linq;
-using System.Web;
-using System.Xml.Schema;
 using DeveloperUniversity.Models;
 
 namespace DeveloperUniversity.DAL
