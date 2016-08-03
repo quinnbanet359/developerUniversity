c5ababd99df471f36b252f802af3568d536fd0e0
Tyler Blake <tyler.blake.louisville@gmail.com>
2016-08-02 22:55:50 -0400

    added viewModels, started adding properties to viewModels

--- DeveloperUniversity/DeveloperUniversity/Models/StudentViewModel.cs
+++ DeveloperUniversity/DeveloperUniversity/Models/StudentViewModel.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DeveloperUniversity.Models
 {
-    public class Student
+    public class StudentViewModel
     {
         public int Id { get; set; }
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int ZipCode { get; set; }
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-
-        public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
