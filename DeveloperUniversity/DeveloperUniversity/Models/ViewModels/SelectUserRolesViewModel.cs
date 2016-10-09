96bb65b4ed08208da8de1c200bc8ea78cf078755
Tyler Blake <tyler.blake.louisville@gmail.com>
2016-10-09 00:14:53 -0400

    initial commit of usermanagement, index page works, edit functionality is still in progress.

---
+++ DeveloperUniversity/DeveloperUniversity/Models/ViewModels/SelectUserRolesViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DeveloperUniversity.Models.ViewModels
+{
+    public class SelectUserRolesViewModel
+    {
+            public SelectUserRolesViewModel()
+            {
+                this.Roles = new List<SelectRoleEditorViewModel>();
+            }
+
+            // Enable initialization with an instance of ApplicationUser:
+            public SelectUserRolesViewModel(ApplicationUser user)
+                : this()
+            {
+                this.UserName = user.UserName;
+                //this.FirstName = user.FirstName;
+                //this.LastName = user.LastName;
+
+                var Db = new ApplicationDbContext();
+
+                // Add all available roles to the list of EditorViewModels:
+                var allRoles = Db.Roles.ToList();
+                foreach (var role in allRoles.ToList())
+                {
+                    // An EditorViewModel will be used by Editor Template:
+                    var rvm = new SelectRoleEditorViewModel(role);
+                    this.Roles.Add(rvm);
+
+                var checkUserRole =
+                    this.Roles.Find(r => r.RoleId == role.Id);
+                checkUserRole.Selected = true;
+            }
+        }
+
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public List<SelectRoleEditorViewModel> Roles { get; set; }
+    }
+}
